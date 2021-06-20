8a814d2acf60b3029882d989fb5856012277842e
Burak KALAYCI <torukobyte@gmail.com>
2021-06-21 02:55:30 +0300

    lincese file created and some unnecessary files(from ready to use template) removed!

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
 import App from "./App";
 
-// layoutsa
-
-// views without layouts
-
-
 ReactDOM.render(
     <App/>,
     document.getElementById("root")
